e258234fc948a17eed60e233328b18c35344e359
yoggsoft <yoggsoft@gmail.com>
2025-02-14 12:23:29 +0100

    delete comments

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -78,27 +78,3 @@ export default function Intro() {
     </section>
   );
 }
-
-// /* The .image-container class needs to be applied to a div wrapped around the image you want to apply this filter to */
-// .image-container {
-//   display: inline-block;
-//   position: relative;
-//   line-height: 0;
-//   background: #42a4ff;
-// }
-// .image-container::after {
-//   content: '';
-//   position: absolute;
-//   width: 100%;
-//   height: 100%;
-//   top: 0;
-//   left: 0;
-//   mix-blend-mode: lighten;
-//   background: #ab0000;
-//   opacity: 0.83;
-// }
-// .image-container > img {
-  // -webkit-filter:   contrast(120%) grayscale(100%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(0%);
-  //         filter:   contrast(120%) grayscale(100%) hue-rotate(0deg) invert(0%) opacity(100%) saturate(100%) sepia(0%);
-  // mix-blend-mode: multiply;
-// }
